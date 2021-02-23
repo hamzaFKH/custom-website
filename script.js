405a3a3d69fd44c4d9561a22a19bd79cb815e0ed
hamzaFKH <hmizecrafter@gmail.com>
2021-02-23 21:06:07 +0100

    Navigation window + javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+const navOpen = document.querySelector('.navOpen');
+
+const nav = document.querySelector('.nav');
+
+
+function openNav(){
+  nav.style.width = '250px';
+};
+function closeNav(){
+  nav.style.width = '0px';
+};
+
+
+(function setup(){
+  console.warn('setup initiation successful.');
+}());
